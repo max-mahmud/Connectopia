6032a7953e85c3eaf9d47d2af472a4efcc1d77c1
max-mahmud <120459397+max-mahmud@users.noreply.github.com>
2023-11-29 11:01:25 +0600

    server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import helmet from "helmet";
+import dbConnection from "./dbConfig/index.js";
+import router from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8800;
+dbConnection();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
